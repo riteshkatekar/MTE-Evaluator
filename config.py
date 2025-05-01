b69189207141672754a7f74f7ee34c603e310f30
riteshkatekar <145281086+riteshkatekar@users.noreply.github.com>
2025-05-01 16:10:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,11 +4,11 @@ ENV = "local"  # ← Switch to "local" for personal Gmail testing
 
 CONFIG = {
     "local": {
-        "central_authority_email": "",
+        "central_authority_email": "",# paste your own mail
         "user_id": "me",  # Local Gmail alias
     },
     "production": {
-        "central_authority_email": "",
+        "central_authority_email": "", # guruji foundation mail
         "user_id": "",  # Guruji Foundation account
     }
 }
